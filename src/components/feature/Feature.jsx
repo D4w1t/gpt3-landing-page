cd1fbc2b36405c4f47e257de43ee1a11f25b06f0
Dawit Nigussie <dawit9n@gmail.com>
2025-07-03 20:48:52 +0300

    feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,18 @@
-import styles from './feature.module.css'
+import styles from "./feature.module.css";
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div>Feature</div>
-  )
-}
+    <div className={styles["gpt3__features-container__feature"]}>
+      <div className={styles["gpt3__features-container__feature-title"]}>
+        <div />
+        <h1> {title} </h1>
+      </div>
 
-export default Feature
+      <div className={styles["gpt3__features-container_feature-text"]}>
+        <p> {text} </p>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
