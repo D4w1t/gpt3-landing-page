59584fe8cae6b6947689855d9a551cb166c3f10c
Dawit Nigussie <dawit9n@gmail.com>
2025-07-03 19:29:42 +0300

    Brand Component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,27 @@
-import styles from './brand.module.css'
+import styles from "./brand.module.css";
+
+import { atlassian, dropbox, google, shopify, slack } from "./imports";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className={`${styles["gpt3__brand"]} section__padding`}>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
