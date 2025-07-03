5893a460d8c46890b4348999fbe4622371fe637d
Dawit Nigussie <dawit9n@gmail.com>
2025-07-03 21:39:02 +0300

    Possibility container

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,32 @@
-import styles from './possibility.module.css';
+import styles from "./possibility.module.css";
+
+import possibilityImage from "../../assets/possibility.png";
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
-  )
-}
+    <div
+      className={`${styles["gpt3__possibility"]}`}
+      id="possibilities"
+    >
+      <div className={styles["gpt3__possibility-image"]}>
+        <img src={possibilityImage} alt="possibility image" />
+      </div>
 
-export default Possibility
+      <div className={styles["gpt3__possibility-content"]}>
+        <h4>Request Early Access To Get Started</h4>
+        <h1 className="gradient__text">
+          The Possibilities are beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access To Get Started</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Possibility;
