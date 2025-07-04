022ac6ef5b91c4951361a72adae2be065d749161
Dawit Nigussie <dawit9n@gmail.com>
2025-07-04 13:43:54 +0300

    Article Component

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
-import styles from './article.module.css';
+import styles from "./article.module.css";
 
-const Article = () => {
+const Article = ({ artImag, imgAlt, date, title }) => {
   return (
-    <div>Article</div>
-  )
-}
+    <div className={styles["gpt3__blog-container_article"]}>
+      <div className={styles["gpt3__blog-container_article-image"]}>
+        <img src={artImag} alt={imgAlt} />
+      </div>
+      <div className={styles["gpt3__blog-container_article-content"]}>
+        <div>
+          <p> {date} </p>
+          <h3> {title} </h3>
+        </div>
+        <p> Read Full Article </p>
+      </div>
+    </div>
+  );
+};
 
 export default Article;
