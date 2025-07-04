3d7b0a0b77d0ac083accb2c7d73022a12e068fbd
Dawit Nigussie <dawit9n@gmail.com>
2025-07-04 14:05:51 +0300

    Footer Container

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,55 @@
-import styles from './footer.module.css';
+import styles from "./footer.module.css";
+
+import logo from "../../assets/logo.svg";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={`${styles["gpt3__footer"]} section__padding`}>
+      <div className={styles["gpt3__footer-heading"]}>
+        <h1 className="gradient__text">
+          Do you want to step in to the Future before others
+        </h1>
+      </div>
 
-export default Footer
+      <div className={styles["gpt3__footer-btn"]}>
+        <p>Request Early Access to Get Started</p>
+      </div>
+
+      <div className={styles["gpt3__footer-links"]}>
+        <div className={styles["gpt3__footer-links_logo"]}>
+          <img src={logo} alt="logo" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. All Rights
+            Reserved
+          </p>
+        </div>
+
+        <div className={styles["gpt3__footer-links_div"]}>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className={styles["gpt3__footer-links_div"]}>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className={styles["gpt3__footer-links_div"]}>
+          <h4>Get In Touch</h4>
+          <p>Lorem ipsum dolor sit amet consectetur</p>
+          <p>086-131-1211-21</p>
+          <p>info@gpt3.ai</p>
+        </div>
+      </div>
+
+      <div className={styles["gpt3__footer-copyright"]}>
+        <p> 2025 GPT-3. All Rights Reserved </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
