f6d56f3db0ad1e1ac79c37f95c460a80815e7671
Dawit Nigussie <dawit9n@gmail.com>
2025-07-04 13:10:08 +0300

    CTA Component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,16 @@ import styles from './cta.module.css'
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className={styles["gpt3__cta"]}>
+      <div className={styles["gpt3__cta-content"]}>
+        <p>Request Early Access to Get Started</p>
+        <h3>Request Today & Start Exploring the Endless Possibilities</h3>
+      </div>
+
+      <div className={styles["gpt3__cta-btn"]}>
+        <button>Get Started</button>
+      </div>
+    </div>
   )
 }
 
